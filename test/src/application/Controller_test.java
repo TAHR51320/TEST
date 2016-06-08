ddb558bc620db26a0cd86cef41b40376716e6be9
Acer <Acer@192.168.0.22>
2016-06-08 23:32:26 +0300

    //

---
+++ test/src/application/Controller_test.java
@@ -0,0 +1,37 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+
+public class Controller_test implements Initializable {
+	@FXML ListView <String> test ;
+	@FXML ObservableList<String> t;
+	
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+	t = FXCollections.observableArrayList("1", "2", "3", "4");
+	test.setItems(t);
+	test.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+	
+	}
+	
+	public void a(ActionEvent e){
+		String list;
+		list = test.getSelectionModel().getSelectedItem();
+		
+	System.out.println(list);
+	}
+
+	
+	
+	
+}
